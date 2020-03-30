a5bee0a25b4719a71c9d6d80ed9108c0b291b79b
aparna4scholas <aparna4scholas@gmail.com>
2020-03-29 20:44:38 -0700

    modified

--- pawtastic/src/App.js
+++ pawtastic/src/App.js
@@ -1,14 +1,43 @@
-import React from 'react';
-
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {petname: ",breed: ",}
+   
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({[event.target.name]: event.target.value})
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
     
-    </div>
-    
-  );
+    alert(`A name was submitted: ${this.state.petname}`);
+    alert(`A name was submitted: ${this.state.breed}`);
+
+  } 
+
+  render() {
+    return (
+      <form onSubmit={this.handleChange}>
+        <h2>  yey,we love dogs!Give us the basics about your pup.</h2>
+        <label>  
+          Name: 
+        <input name="petname"  value={this.state.name} onChange={this.handleChange}/>
+         </label>
+        <label>  
+          breed: 
+      <input name="breed"  value={this.state.breed} onChange={this.handleChange}/>
+          </label>
+        <input type="submit"  value="Submit" onClick={this.handleSubmit}/>
+      </form>
+    )
+  }
 }
-
-export default App;
